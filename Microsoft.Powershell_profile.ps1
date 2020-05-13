bfe22e50ea3eb129d3f4ef311c953c30bead27d7
Jonathan Markman <jmarkman446@gmail.com>
2020-05-13 02:49:39 -0400

    add variables, check for path, call ffmpeg

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -38,3 +38,26 @@ function Update-NeoCities([string] $For)
 
     Pop-Location
 }
+
+function New-Gif ([string] $VideoFile, [int] $StartTime, [int] $Duration, [string] $OutputFilename = "output.gif") 
+{
+    $ffpmegPaletteFolder = C:\Users\jon\AppData\Local\Temp
+    $paletteFolderName = "ffmpeg-palette"
+    $paletteFilename = "palette.png"
+    $paletteLocation = Join-Path -Path $ffpmegPaletteFolder -ChildPath $paletteFolderName
+    $paletteOutput = Join-Path -Path $paletteLocation -ChildPath $paletteFilename
+    $outputDirectory = C:\Users\jon\Pictures
+    $output = Join-Path -Path $outputDirectory -ChildPath $OutputFilename
+    $filters = "fps=15,scale=320:-1:flags=lanczos"
+    $ffmpeg = 'C:\Program Files\ffmpeg\ffmpeg.exe'
+
+    # Check to see that we've got a folder to drop the palette in; if we don't, make one
+    # TodayILearned: https://stackoverflow.com/a/50366338
+    if (-not (Test-Path $paletteLocation))
+    {
+        New-Item -Path $ffpmegPaletteFolder -Name $paletteFolderName -ItemType "directory"
+    }
+    
+    & $ffmpeg -v warning -ss $StartTime -t $Duration -i $VideoFile -vf "$filters,palettegen" -y $paletteOutput
+    & $ffmpeg -v warning -ss $StartTime -t $Duration -i $VideoFile -i $paletteOutput -lavfi "$filters [x]; [x][1:v] paletteuse" -y $output
+}
